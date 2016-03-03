f5a16e98e5cc0cb9d3683c2c068e71ea029dab26
Jeyavinoth Jeyaratnam <jeyavinoth@gmail.com>
2016-03-03 17:31:59 -0500

    initial empty commit

---
+++ stations.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+def getStationList(filename):
+  """Reading the inventory file to get the list of stations, for now only supports csv format"""
+
+  check if file exists
+  f = open(filename,'r')
+
+  close(f)
+  stationList = filename 
+  return stationList 
+
+def extractStationData(filename):
+  return np.arange(0,10,1)
+
